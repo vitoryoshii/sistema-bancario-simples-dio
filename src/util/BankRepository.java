3843aa867c782f08fd6927a4af82111f76cc025e
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-09-03 10:21:58 -0300

    feat: Create refactor: separate client and manager management into BankRepository

---
+++ src/util/BankRepository.java
@@ -0,0 +1,55 @@
+package util;
+
+import main.MainSystemBank;
+import models.Client;
+import models.Manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankRepository {
+    private List<Client> clients;
+    private List<Manager> managers;
+
+    public BankRepository() {
+        this.clients = new ArrayList<>();
+        this.managers = new ArrayList<>();
+    }
+
+    // Methods get e set to Lists Clients and Manager
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public List<Manager> getManagers() {
+        return managers;
+    }
+
+    public void addClient(Client client) {
+        this.clients.add(client);
+    }
+
+    public void addManager(Manager manager) {
+        this.managers.add(manager);
+    }
+
+    // Method search customer by CPF
+    public Client searchCustomerByCPF(String cpf) {
+        for (Client c : getClients()) {
+            if (c.getCpf().equals(cpf)) {
+                return c; // return client found
+            }
+        }
+        return null; // return null
+    }
+
+    // Method search manager by CPF
+    public Manager searchManagerByCPF(String cpf) {
+        for (Manager m : getManagers()) {
+            if (m.getCpf().equals(cpf)) {
+                return m; // return manager found
+            }
+        }
+        return null; // return null
+    }
+}
